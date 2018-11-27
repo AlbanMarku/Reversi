efe07a9109e9d2ce5002f4bb8bdb33e6ab40a509
AlbanMarku <43135902+AlbanMarku@users.noreply.github.com>
2018-11-27 13:26:25 +0000

    Add files via upload

--- src/reversi/Board.java
+++ src/reversi/Board.java
@@ -10,7 +10,7 @@ public class Board {
 	/**
 	 * Creating board
 	 */
-	char[][] grid = { { '.', '.', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '.', '.', '.' }, 
+	char[][] grid = { { '.', '.', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '.', '.', '.' },
 			{ '.', '.', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', 'w', 'b', '.', '.', '.' },
 			{ '.', '.', '.', 'b', 'w', '.', '.', '.' }, { '.', '.', '.', '.', '.', '.', '.', '.' },
 			{ '.', '.', '.', '.', '.', '.', '.', '.' }, { '.', '.', '.', '.', '.', '.', '.', '.' } };
@@ -42,7 +42,8 @@ public class Board {
 
 	public void boardCheck() {
 		/**
-		 * Displays if the board is full. Will be used to determine if the game is over later.
+		 * Displays if the board is full. Will be used to determine if the game is over
+		 * later.
 		 */
 		boolean full = true;
 
@@ -71,7 +72,8 @@ public class Board {
 		System.out.println("number2");
 		choice2 = userNumbers2.nextInt();
 		/**
-		 * After the numbers are entered, the valid move method is called to see if the move is legal.
+		 * After the numbers are entered, the valid move method is called to see if the
+		 * move is legal.
 		 */
 		validMove();
 		userNumbers.close();
@@ -83,39 +85,155 @@ public class Board {
 		 * Swaps player each turn.
 		 */
 		if (player == 'w') {
+			System.out.println("------");
+			System.out.println("b turn");
+			System.out.println("------");
 			player = 'b';
 		} else {
+			System.out.println("------");
+			System.out.println("w turn");
+			System.out.println("------");
 			player = 'w';
 		}
 	}
 
 	public void validMove() {
-		if (grid[choice1 - 2][choice2 - 2] == 'w') { // This is checking the top left of the players move to see if it's 'w'. I thought I could put a null value in one of the [] so it only checks the left or right but then Java doesn't like that. So for now I can only do diagonals. Maybe I could do [coice1 - choice1] to make it null?
-			grid[choice1 - 1][choice2 - 1] = player; // Places the piece if move is valid. It's -1 just to not confuse the user. Otherwise if they want to put something at 1,1 they would need to write 0,0.
-			currentPlayer(); //Swaps turn.
+		
+		if (grid[choice1 - 2][choice2 - 1] == 'w' && player == 'b') { // up
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer(); // Swaps turn.
 			boardLayout(); // Updates board.
-		} else {
-			System.out.println("nope");//If invalid it will say nope and go back for the user to try again.
+		}
+
+		if (grid[choice1][choice2 - 1] == 'w' && player == 'b') { // down
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 1][choice2 - 2] == 'w' && player == 'b') { // left
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 1][choice2] == 'w' && player == 'b') { // right
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 2][choice2 - 2] == 'w' && player == 'b') { // up-left
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 2][choice2] == 'w' && player == 'b') { // up-right
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1][choice2 - 2] == 'w' && player == 'b') { // down-left
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1][choice2] == 'w' && player == 'b') { // down-right
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+		
+		if (grid[choice1 - 2][choice2 - 1] == 'b' && player == 'w') { // up
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer(); // Swaps turn.
+			boardLayout(); // Updates board.
+		}
+
+		if (grid[choice1][choice2 - 1] == 'b' && player == 'w') { // down
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 1][choice2 - 2] == 'b' && player == 'w') { // left
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 1][choice2] == 'b' && player == 'w') { // right
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 2][choice2 - 2] == 'b' && player == 'w') { // up-left
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1 - 2][choice2] == 'b' && player == 'w') { // up-right
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1][choice2 - 2] == 'b' && player == 'w') { // down-left
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		if (grid[choice1][choice2] == 'b' && player == 'w') { // down-right
+			grid[choice1 - 1][choice2 - 1] = player;
+
+			currentPlayer();
+			boardLayout();
+		}
+
+		else {
+			System.out.println("nope");// If invalid it will say nope and go back for the user to try again.
 			chooseMove();
 		}
 	}
 
 	public void saveGame() {
 		/**
-		 * I'm trying to save the grid if players want to play another time. Doesn't work right now. The file it creates is blank.
+		 * I'm trying to save the grid if players want to play another time. Doesn't
+		 * work right now. The file it creates is blank.
 		 */
 		FileOutputStream outputStream;
 		PrintWriter printWriter = null;
-		
-				try {
-			
+
+		try {
+
 			outputStream = new FileOutputStream("saveGame");
 			printWriter = new PrintWriter(outputStream);
-			
-		do {
-			printWriter.println(grid);
-		} while (outputStream != null);
-			
+
+			do {
+				printWriter.println(grid);
+			} while (outputStream != null);
+
 		} catch (Exception e) {
 			System.out.println("error");
 		}
